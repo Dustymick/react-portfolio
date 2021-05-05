471d976bdb3a2111b53217f38db8b1a11cbcf404
Dustin Micklos <damicklo5@gmail.com>
2021-05-05 14:19:35 -0500

    footer added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from "./components/Nav";
 import About from './components/About';
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <About/>
       <Projects/>
       <Contact/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+      <footer>
+        <p>Created by Dustin Micklos</p>
+      </footer>
+    )
+}
+
+export default Footer
